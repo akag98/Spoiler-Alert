67538f9ba72b5971eb2c375173aa21968fc6311a
akag98 <38176157+akag98@users.noreply.github.com>
2018-10-03 19:33:04 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,21 +2,6 @@ jQuery.expr[':'].contains = function(a, i, m) {
     return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
 }; 
 
-//IMPORTANT
-// chrome.runtime.onMessage.addListener(
-//     function(request, sender, sendResponse) {
-//         var name = request.greeting;
-//         highlight(name);
-//     //   console.log(sender.tab ?
-//     //               "from a content script:" + sender.tab.url :
-//     //               "from the extension");
-//     //   if (request.greeting == "hello")
-//     //     sendResponse({farewell: "goodbye"});
-//     }
-
-// );
-
-
 chrome.storage.onChanged.addListener(function(changes, namespace) {
     chrome.storage.sync.get(['name'], function(result) {
         //console.log('Array Values currently are  ' + result.name);
@@ -52,14 +37,7 @@ function highlight(name){
 	
     var element = $("p:contains(" + name + ")");
     for (i = 0; i < element.length; i++) {
-        //console.log(element[i]);
-        // var x = document.createElement("BUTTON");
-        // var t = document.createTextNode("Show");
-        // x.appendChild(t);
-        // x.setAttribute("onclick", "hideOrShow(element[i])");
-        // element[i].appendChild(x);
         element[i].parentNode.removeChild(element[i]);
-        //$(element[i]).parent().closest('div').css("background-color", "red");
     }
 
     var element = $("h1:contains(" + name + ")");
